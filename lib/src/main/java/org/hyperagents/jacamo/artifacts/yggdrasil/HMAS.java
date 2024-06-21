31a5630240680138d64efe6fbfae360d6e898048
V4L3 <V4L3@users.noreply.github.com>
2024-06-21 12:11:01 +0200

    refactor(artifacts): add documentation

--- lib/src/main/java/org/hyperagents/jacamo/artifacts/yggdrasil/HMAS.java
+++ lib/src/main/java/org/hyperagents/jacamo/artifacts/yggdrasil/HMAS.java
@@ -3,6 +3,10 @@ package org.hyperagents.jacamo.artifacts.yggdrasil;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 
+/**
+ * The HMAS class represents the Hypermedia Multi-Agent System (HMAS).
+ * It provides a set of constants for the HMAS vocabulary.
+ */
 public final class HMAS {
   public static final String PREFIX = "https://purl.org/hmas/";
 
